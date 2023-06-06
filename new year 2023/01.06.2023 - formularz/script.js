27f6a7bbd9e3098a7a87ab568dd251e44069fa74
pytongi <pythongowo@gmail.com>
2023-06-06 09:17:16 +0200

    made a form with checking inputs

--- new year 2023/01.06.2023 - formularz/script.js
+++ new year 2023/01.06.2023 - formularz/script.js
@@ -5,18 +5,21 @@ function check()
     let wiek = document.getElementById('wiek').value;
     let email = document.getElementById('email').value;
 
-
     console.log(imie, nazwisko, wiek, email);
 
-    if (imie == '' || nazwisko == '' || wiek == '' || email == '')
+    if (imie == '' || nazwisko == '' || wiek == '' || email == '' || !email.includes("@"))
     {
         alert("usupełnij wszystkie okna");  
         return 0;              
     }
-    else (imie != '' && nazwisko != '' && wiek != '' && email != '')
+    else if (imie.length < 2 || nazwisko.length < 2 || wiek < 18 || !email.includes("@"))
+    {
+        alert("nie spełniasz wymagań");  
+        return 0; 
+    }
+    else
     {
         alert("wysłano formularz");
         return 0; 
     }
-
 }
