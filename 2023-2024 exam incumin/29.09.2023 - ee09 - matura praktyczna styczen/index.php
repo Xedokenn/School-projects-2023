9ac09e8c8b13d037a024d4bd7fcb381ad74bbca1
jacob shool <jaja@example.com>
2023-09-29 11:21:04 +0200

    #

---
+++ 2023-2024 exam incumin/29.09.2023 - ee09 - matura praktyczna styczen/index.php
@@ -0,0 +1,35 @@
+<?php
+$connect = mysqli_connect('localhost','root','','firma');
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sekretariat</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <section id="left">
+        <h1>Akta Pracownicze</h1>
+        <?php
+            $
+        
+        ?>
+        <h3>Dokumenty pracownika</h3>
+        <a href"zad1/cv.txt">Pobierz</a>
+        <?php
+        ?>
+    </section>
+    <section id="right">
+        <?php
+        ?>
+
+    </section>
+    <footer>
+
+    </footer>
+</body>
+</html>
