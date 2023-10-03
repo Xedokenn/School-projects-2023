0bacf4a49ab9c7f23c7057f6b050a9a0932cbd16
Xedoken <kubabuba103@gmail.com>
2023-10-03 15:21:50 +0200

    homework

--- 2023-2024 exam incumin/29.09.2023 - ee09 - matura praktyczna styczen/index.php
+++ 2023-2024 exam incumin/29.09.2023 - ee09 - matura praktyczna styczen/index.php
@@ -1,37 +1,78 @@
-<?php
-$connect = mysqli_connect('localhost','root','','firma');
-?>
-
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sekretariat</title>
-    <link rel="stylesheet" href="styl.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <section id="left">
+    <div id="lewy">
         <h1>Akta Pracownicze</h1>
         <?php
-            $zapytanie = "select imie, nazwisko, adres, miasto, czyRODO, czyBadania from pracownicy where id='2'";
+            $con = mysqli_connect('localhost', 'root', '', 'firma');
+            $q = "SELECT id,imie,nazwisko,adres,miasto,czyRODO,czyBadania FROM pracownicy WHERE id=2;";
+            $res = mysqli_query($con, $q);
+            while ($row = mysqli_fetch_array($res)) {
+                $rodo = ($row[5] == 1) ? 'podpisano' : 'niepodpisano';
+                $badania = ($row[6] == 1) ? 'aktualne' : 'nieaktualne';
+
+
+                echo "
+                <h3>dane</h3>
+                <p>$row[1] $row[2]</p>
+                <hr/>
+                <h3>adres</h3>
+                <p>$row[3]</p>
+                <p>$row[4]</p>
+                <hr/>
+                <p>RODO: $rodo</p>
+                <p>Badania: $badania</p>
+                ";
+            }
+
+
+
+            $q = "SELECT COUNT(*) FROM pracownicy";
+            $res = mysqli_query($con, $q);
+            $row = mysqli_fetch_array($res);
+        ?>
+            <a href="cv.txt" target="_blank">Pobierz</a>
+        <?php
+
+            echo "
+                  <h1>Liczba zatrudnionych pracowników</h1>
+                  <p>$row[0]</p>
+                  ";
+        ?>
+    </div>
+    <div id="prawy">
+        <?php
+            $q = "SELECT id,imie,nazwisko FROM pracownicy WHERE id=2;";
+            $res = mysqli_query($con, $q);
+            while ($row = mysqli_fetch_array($res)) {
+                echo "<img src='$row[0].jpg' alt='pracownik'>
+                      <h2>$row[1] $row[2]</h2>";
+            }
+
+            $q = "SELECT pracownicy.id, stanowiska.nazwa, stanowiska.opis FROM pracownicy, stanowiska WHERE pracownicy.stanowiska_id = stanowiska.id AND pracownicy.id = 2;";
+            $res = mysqli_query($con, $q);
+            while ($row = mysqli_fetch_array($res)) {
+                echo "<h4>$row[1]</h4>
+                      <h5>$row[2]</h5>";
+            }
+
+            mysqli_close($con);
+
             
-
-        
         ?>
-        <h3>Dokumenty pracownika</h3>
-        <a href"zad1/cv.txt">Pobierz</a>
-        <?php
-        ?>
-    </section>
-    <section id="right">
-        <?php
-        ?>
-
-    </section>
-    <footer>
-
-    </footer>
+    </div>
+    <div id="stopka">
+        Autorem aplikacji jest: 213742069
+        <ul>
+            <li> Skontaktuj się</li>
+            <li> Poznaj naszą firmę</li>
+        </ul>
+    </div>
 </body>
 </html>
+
