b3db82ca1ec52c700db220f43aecbde80029cefe
Xedoken <kubabuba103@gmail.com>
2023-10-12 14:21:12 +0200

    zaawansowane

--- 2023-2024 exam incumin/12.10.2023 - sortowania w php/index.php
+++ 2023-2024 exam incumin/12.10.2023 - sortowania w php/index.php
@@ -22,32 +22,52 @@ session_start();
                 $tabela = array('12','123','1','5','756','234','43','24');
                 $n = count($tabela);
 
-                for($i=$n;$i>=0;$i--){      
-                    for($j=0;$j<$i-1;$j++){
-                        if($tabela[$j]>$tabela[$j+1]){
-                            $tmp=$tabela[$j];
-                            $tabela[$j]=$tabela[$j+1];
-                            $tabela[$j+1]=$tmp;
+
+                for ($i = $n; $i >= 0; $i--) {      
+                    for ($j = 0; $j < $i - 1; $j++) {
+                        if ($tabela[$j] > $tabela[$j + 1]) {
+                            $g = $tabela[$j];
+                            $tabela[$j] = $tabela[$j + 1];
+                            $tabela[$j + 1] = $g;
                         }
                     }
                 }
 
-                for($i=0;$i<$n;$i++){
+                for ($i = 0; $i < $n; $i++) {
                     echo($tabela[$i].', ');
                 }
             ?>
 
-            <h5> Zaawansowane sortowanie (Heap Sort) : </h5>
+
+            <br><br>
+            <h5> Zaawansowane sortowanie (Quick Sort) : </h5>
             <?php
                 $tabela = array('12','123','1','5','756','234','43','24');
                 $n = count($tabela);
 
+                function sort1($tab){
+                    $loe = $gt = array();
+
+                    if(count($tab) < 2){
+                       return $tab;
+                    }
+                    $klucz = key($tab);
+                    $drift = array_shift($tab);
 
 
+                    foreach($tab as $wartosc) {
+                       if($wartosc <= $drift) {
+                          $loe[] = $wartosc;
 
-                for($i=0;$i<$n;$i++){
-                    echo($tabela[$i].', ');
-                }
+                       }elseif ($wartosc > $drift) {
+                          $gt[] = $wartosc;
+                       }
+                    }
+                    return array_merge(sort1($loe),array($klucz=>$drift), sort1($gt));
+                 }
+
+                 $sorted = sort1($tabela);
+                 print_r($sorted);
             ?>
         </main>
     </div>
