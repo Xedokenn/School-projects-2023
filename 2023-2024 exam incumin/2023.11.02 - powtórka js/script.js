af2b465d30acffb5b221d53a4a5ee2128d1191fe
Xedoken <kubabuba103@gmail.com>
2023-11-02 11:37:00 +0100

    02.11 js split reverse join palindrome

---
+++ 2023-2024 exam incumin/2023.11.02 - powtórka js/script.js
@@ -0,0 +1,32 @@
+function f(color) {
+
+    var fontsize = document.getElementById("procent").value;
+    var fonttype = document.getElementById("styl").value;
+
+    document.getElementById("wynik").style.fontSize = String(fontsize)+"%";
+    document.getElementById("wynik").style.fontStyle = String(fonttype);
+    document.getElementById("wynik").style.color = color;
+
+}  
+
+function podziel() {
+    const text = document.getElementById("napis").value.split(" ");
+    var output = "";
+    for (var i = 0; i < text.length; i++) {
+        output += text[i] + "<br>";
+    }
+    document.getElementById("wynik").innerHTML = output;
+}
+
+function palindrom() {
+    let str = document.getElementById("napis").value;
+    let reversed = str.split("").toReversed().join("");
+    console.log(str);
+    console.log(reversed);
+    if (str == reversed) {
+        document.getElementById("wynik").innerHTML = "tak, to jest palindrom";
+    } else {
+        document.getElementById("wynik").innerHTML = "nie, to nie jest palindrom";
+    }
+    
+}
