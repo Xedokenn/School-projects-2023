4a48059cfad961c0fa32fe18cfed61aa18a7640b
Xedoken <kubabuba103@gmail.com>
2023-11-09 13:21:30 +0100

    done js version

--- 2023-2024 exam incumin/2023.11.09 - js co 20 sekund wyslij kolejne zdjecie/rozwiazanie/grzyby.php
+++ 2023-2024 exam incumin/2023.11.09 - js co 20 sekund wyslij kolejne zdjecie/rozwiazanie/grzyby.php
@@ -8,7 +8,6 @@
 	<meta charset="UTF-8" />
 	<title>Grzybobranie</title>
 	<link rel="stylesheet" href="styl5.css" />
-	<script src="script.js" type="text/javascript" defer></script>
 </head>
 <body>
 	<div id="miniatura">
@@ -19,11 +18,40 @@
 	</div>
 	<div id="lewy">
 		<header id="pole" ></header>
-			<button onclick="main()">Charge website</button>
 			<p id="wynik"></p>
+			<?php 
+				$con = mysqli_connect('localhost', 'root', '', 'dane2');
+        		$name = "Hello World"; 
+    		?> 
+  
+
+    		<script type="text/javascript">
+				var x = "<?php
+					$querry0 = "SELECT id FROM grzyby";
+					if ($result = mysqli_query($con, $querry0)) {
+					    $rowcount = mysqli_num_rows($result);
+					} else {
+						echo "broken";
+					}
+
+
+					if(isset($_SESSION['views']) && $_SESSION['views'] < $rowcount){
+						$_SESSION['views'] = $_SESSION['views']+ 1;
+					 }else{
+						$_SESSION['views'] = 1;
+					 }
+					 $counting = $_SESSION['views'];
+				 
+					$querry1 = "SELECT nazwa_pliku, opis FROM grzyby where id = $counting";
+					$output = mysqli_query($con, $querry1);
+					$row0 = mysqli_fetch_row($output);
+					echo "<img src='$row0[0]'/>";
+				?>"; 
+    		    document.write(x);
+    		</script> 
+			
 	</div>
-	<div id="prawy">
-		
+	<div id="prawy">		
 	</div>
 	<div id="stopka">
 		<p>Autor: PESEL</p>
