742aea6f98745b0e9e669426f356a5d3fe04292a
jacob shool <jaja@example.com>
2023-12-01 11:05:17 +0100

    #

--- DeathModePy/future.py
+++ DeathModePy/future.py
@@ -6,11 +6,11 @@ import asyncio
 #pierwszy przelot, zlicz ilosc bramek + czas
 czas_przelotu = 60 # czas w sekundach
 ilosc_bramek = 0
-
+czas_bramek = []
 power = 1000 # moc 
 ciezkosc = 4 # im wieksza liczba, tym mniej mocy masz 
 #srednia czasu miedzy bramkami
-delta = czas_przelotu / ilosc_bramek
+#delta = czas_przelotu / ilosc_bramek
 
 # Timer starts
 starttime = time.time()
@@ -18,27 +18,44 @@ lasttime = starttime
 lapnum = 1
 value = ""
   
-print("Press ENTER for each lap.\nType Q and press ENTER to stop.")
-while value != "q":       
+# print("Press ENTER for each lap.\nType Q and press ENTER to stop.")
+# while value != "q":       
     
-    if ilosc_bramek > 0: # dla kazdej bramki wyslij value, ktore zliczy czas miedzy bramkami
+#     if ilosc_bramek > 0: # dla kazdej bramki wyslij value, ktore zliczy czas miedzy bramkami
         
-        # The current lap-time
-        laptime = round((time.time() - lasttime), 2)
-        # Total time elapsed since the timer started
-        totaltime = round((time.time() - starttime), 2)
-        # Printing the lap number, lap-time, and total time
-        print("Lap No. "+str(lapnum))
-        print("Total Time: "+str(totaltime))
-        print("Lap Time: "+str(laptime))
-        print("*"*20)
-        # Updating the previous total time and lap number
-        lasttime = time.time()
-        lapnum += 1
-    print("Exercise complete!")
+#         # The current lap-time
+#         laptime = round((time.time() - lasttime), 2)
+#         # Total time elapsed since the timer started
+#         totaltime = round((time.time() - starttime), 2)
+#         # Printing the lap number, lap-time, and total time
+#         print("Lap No. "+str(lapnum))
+#         print("Total Time: "+str(totaltime))
+#         print("Lap Time: "+str(laptime))
+#         print("*"*20)
+#         # Updating the previous total time and lap number
+#         lasttime = time.time()
+#         lapnum += 1
+#     print("Exercise complete!")
+
+
+# for bramka in range(ilosc_bramek):
+#     ilosc_bramek += ilosc_bramek[bramka]
         
-        
-        
+switch_flag = False
+start_time = 0
+end_time = 0
+
+def switch():
+    global switch_flag, start_time
+    switch_flag = True
+    start_time = time.time()
+
+def unswitch():
+    global switch_flag, start_time, end_time
+    switch_flag = False
+    end_time = time.time()
+    if end_time - start_time > 5:
+        print("5 seconds elapsed!")
         
 # #kazda bramka daje power/ciezkosc
 # #jezeli czas czas_przelotu/(ciezkosc*2), power = 0
