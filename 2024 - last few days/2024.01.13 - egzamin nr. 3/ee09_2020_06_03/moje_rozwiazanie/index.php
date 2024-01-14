d13383a0c7eda93582e8c7234fae48ec1a06eec2
Xedoken <kubabuba103@gmail.com>
2024-01-14 18:39:56 +0100

    14.01.not enough time to do all

---
+++ 2024 - last few days/2024.01.13 - egzamin nr. 3/ee09_2020_06_03/moje_rozwiazanie/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mój kalendarz</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <header>
+        <img src="logo1.png" alt="Mój kalendarz">
+    </header>
+    <header>
+        <h1>KALENDARZ</h1>
+        <!-- skrypt 1 -->
+        <?php
+
+        ?>
+    </header>
+    <main>
+        <!-- skrypt2 -->
+        <?php
+
+
+            echo "<section></section>";
+        ?>
+    </main>
+    <footer>
+        <form method="post" action="index.php">
+            <label for="wpis">dodaj wpis:</label>
+            <input type="text" id="wpis" name="wpis" value="wpis">
+            <input type="submit" id="wpis" name="wpis" value="wpis">
+
+        </form>
+    </footer>
+    
+</body>
+</html>
